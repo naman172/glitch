eb23df3475a467a3f7e50dc0d73143c724abdf08
unknown <naman1722000@gmail.com>
2021-06-09 00:15:23 +0530

    error fixes 3

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 //Components
 import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/ToolBar";
+import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 import ExploreIcon from "@material-ui/icons/Explore";
 import ToolTip from "@material-ui/core/Tooltip";
